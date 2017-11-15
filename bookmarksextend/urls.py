02a213e1149c1eeba83794cb637b34e7b5adc9a8
achiengcindy <achiengcindy36@gmail.com>
2017-11-15 03:52:21 -0500

    creating model Profiles and configuring static paath

--- bookmarksextend/urls.py
+++ bookmarksextend/urls.py
@@ -14,8 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
 from django.conf.urls import url, include
-
 from django.contrib import admin
+
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 urlpatterns = [
@@ -23,3 +25,5 @@ urlpatterns = [
     url(r'^account/', include('account.urls')),
     
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
