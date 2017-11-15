f267a0f817353694db16e87eadc2df4d0bbf8d45
achiengcindy <achiengcindy36@gmail.com>
2017-11-15 02:07:31 -0500

    change passward authentication

--- account/urls.py
+++ account/urls.py
@@ -17,14 +17,11 @@ urlpatterns = [
     url(r'^login/$', auth_views.login, name='login'),
     url(r'^logout/$', auth_views.logout, name='logout'),
     url(r'^logout-then-login/$', auth_views.logout_then_login, name='logout-then-login'),
-    
-    #url(r'^login/$', login, {'template_name': 'registration/login.html'}, name='login'),
     #url(r'^logout/$', logout, {'template_name': 'registration/logged_out.html'}, name='logout'),
-    #url(r'^logout-then-login/$',logout-then-login,{'template_name': 'logout-then-login.html'}, name='mysite_logout-then-login')
-
+   
     # change password urls
-   # url(r'^password-change/$', 'django.contrib.auth.views.password_change', name='password_change'),
-   # url(r'^password-change/done/$', 'django.contrib.auth.views.password_change_done', name='password_change_done'),
+    url(r'^password-change/$', auth_views.password_change,name='password_change'),
+    url(r'^password-change/done/$', auth_views.password_change_done,name='password_change_done'),
 
     # restore password urls
     #url(r'^password-reset/done/$', 'django.contrib.auth.views.password_reset_done', name='password_reset_done'),
