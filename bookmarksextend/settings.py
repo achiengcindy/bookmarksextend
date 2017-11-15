9b8ac346413e526133a6f59d3e28d26eca6895f5
achiengcindy <achiengcindy36@gmail.com>
2017-11-15 02:38:17 -0500

    password resets via email

--- bookmarksextend/settings.py
+++ bookmarksextend/settings.py
@@ -15,6 +15,12 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+#EMAIL_HOST = 'smtp.gmail.com'
+#EMAIL_HOST_USER = 'achiengcindy36@gmail.com'
+#EMAIL_HOST_PASSWORD = 'kenDra#95'
+#EMAIL_PORT = 587
+#EMAIL_USE_TLS = True
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
